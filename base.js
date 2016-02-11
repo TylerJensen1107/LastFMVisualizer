9b8c17fdd23e2f55751cea8ffbeeaef46bc5cbbe
Nathalie McGovern <nathalie.mcgovern@gmail.com>
2016-02-10 17:19:19 -0800

    formatted date. Set key of array to date

--- base.js
+++ base.js
@@ -1,16 +1,19 @@
 console.log("here");
 
 //Set up D3 graph
-var margin = {top: 30, right: 20, bottom: 30, left: 50},
-    width = 600 - margin.left - margin.right,
-    height = 270 - margin.top - margin.bottom;
+var margin = {top: 40, right: 20, bottom: 30, left: 50},
+	// 
+    width = 1000 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 // Parse the date / time
 var parseDate = d3.time.format("%d-%b-%y").parse;
 
+var maxWeeks = 50;
+
 // Set the ranges
-var x = d3.time.scale().range([0, width]);
-var y = d3.scale.linear().range([height, 0]);
+var x = d3.time.scale().range([0, 50 * 12]);	// x axis
+var y = d3.scale.linear().range([height, 0]);	// y axis
 
 // Define the axes
 var xAxis = d3.svg.axis().scale(x)
@@ -34,6 +37,7 @@ var svg = d3.select("body")
     .y(function(d) { return y(d.playCount); });
 
 var dataArray = [];
+var dateArray = [];
 
 //Last FM API calls
 $.ajax({
@@ -47,19 +51,37 @@ $.ajax({
 			url: "http://ws.audioscrobbler.com/2.0/?method=user.getWeeklyArtistChart&api_key=5e2801138b4ef76aeb794a9469cb3687&user=tylerjensen1107&format=json&from=" + from + "&to=" + to,
 
 		}).done(function(data) {
-				console.log(data);
+				//console.log(data);
 			$.each(data.weeklyartistchart.artist, function(index, weeklyData) {
-				if(!dataArray[weeklyData.name])
-					dataArray[weeklyData.name] = [];
- 					dataArray[weeklyData.name].push({date: data.weeklyartistchart['@attr'].to, 
-												 playCount: weeklyData.playcount});
+				//console.log(weeklyData);
+
+				// if(!dataArray[weeklyData.name])
+				// 	dataArray[weeklyData.name] = [];
+ 			// 	dataArray[weeklyData.name].push({date: data.weeklyartistchart['@attr'].to, 
+				// 								playCount: weeklyData.playcount});
+
+				// Converts string of date in seconds to MM/DD/YYYY
+				var date = new Date(data.weeklyartistchart['@attr'].to * 1000)
+					.toLocaleDateString();
+
+ 				if(!dataArray[date])
+					dataArray[date] = [];
+ 				dataArray[date].push({name: weeklyData.name, 
+												playCount: weeklyData.playcount,
+												weekDate: date,
+												dateInSeconds: data.weeklyartistchart['@attr'].to});
 			});
 
 			console.log(dataArray);
-			
 		});
+		console.log(dataArray.length);
 	});
 
+});
+
+dataArray.forEach(function(d) {
+    d.date = parseDate(d.date);
+    d.close = +d.close;
 });
 
 svg.append("g")
